Added backprops. There is probably something wrong right there

zadanie3/conv_net/my_conv/layers/conv.py
@@ -4,8 +4,12 @@ import numpy as np
 
 
 class Conv(Layer):
-    def __init__(self, input_dims: tuple, output_dims: tuple):
+    def __init__(self, activation_fcn: str, input_dims: tuple, output_dims: tuple):
         super().__init__("conv", input_dims, output_dims)
+        act_dic = {"relu": self.relu}
+        act_div_dic = {self.relu: self.relu_derivative}
+        self.activation_fcn = act_dic[activation_fcn]
+        self.activation_fcn_der = act_div_dic[self.activation_fcn] 
         
     def forward(self, input: np.array, weights: np.array, bias: np.array):
         """Conv net operation
@@ -28,7 +32,7 @@ class Conv(Layer):
         #print(input.shape)
         temp_shape = input.shape
         #print(f"Dlugosc rozmiary{weights.shape}")
-        out_dims = (temp_shape[0], temp_shape[1]-weights.shape[0]+1, temp_shape[2]-weights.shape[1]+1, weights.shape[3])
+        out_dims = self.output_dims
         # Dimensions of output. They are defined by weight array
         out_arr: np.array
         out_arr = np.zeros(out_dims)
@@ -50,7 +54,145 @@ class Conv(Layer):
                         res: np.float64
                         res = np.sum(np.multiply(image[k:k+n, l:l+n, :], filt)) + bias[j]
                         out_arr[i][k][l][j] = res
-        self.activation_val = self.output = out_arr
+        self.activation_val = out_arr
+        self.output = self.relu(self.activation_val)
+        return self.output
+    def back(self, prop: np.array, prev_output: np.array, weights: np.array, bias: np.array, next_weights: np.array, lr: np.float64):
+        print("Start conv")
+        loss_derivative: np.array
+        loss_derivative = prop
+        out_derivative: np.array
+        activation_derivative: np.array
+        # for k in range(loss_derivative.shape[0]):
+        #     for i in range(loss_derivative.shape[1]):
+        #         # For all neurons of current layer
+        #         for j in range(prop.shape[1]):
+        #             # Loss propagating from next layer
+        #             loss_derivative[k][i] += next_weights[j][i] * prop[k][j]
+        #             # Temp_der times weight connecting it to our neuron 
+
+
+        out_derivative = self.activation_fcn_der(self.activation_val)
+        #print(prop.shape)
+        ## b) Calculate delta(lay_out)/delta(activation)
+
+        activation_derivative = self.activation_derivative(weights, prev_output)
+        # c) Calculate delta(activation)/delta(weights)
+
+        prop = np.multiply(loss_derivative, out_derivative)
+        #print(prop.shape)
+        gradients = np.zeros((prev_output.shape[0], weights.shape[0], weights.shape[1], weights.shape[2], weights.shape[3]))
+        #print(gradients.shape)
+        for batch in range(gradients.shape[0]): #2 batch
+            for x in range(activation_derivative.shape[1]): # 3 output
+                for y in range(activation_derivative.shape[2]): # 3 output
+                    for i in range(gradients.shape[1]): #3 w
+                        for j in range(gradients.shape[2]): #3 w
+                            for k in range(gradients.shape[3]): #64 w
+                                for channel in range(gradients.shape[3]): #64 channel
+                                    gradients[batch][i][j][k][channel] += activation_derivative[batch][x][y][i][j][k][channel] * prop[batch][x][y][channel]
+
+
+        arr = np.zeros(self.input_dims)
+
+
+        for batch in range(arr.shape[0]):
+            for i in range(prop.shape[1]): # Move right in prop
+                for j in range(prop.shape[2]): # Move left in prop
+                    for channel in range(prop.shape[3]):
+                        arr[batch, i:i+3, j:j+3, :] += prop[batch][i][j][channel] *  weights[..., channel]
+
+        prop = arr
+        return gradients, prop
+
+
+
+
+    def relu(self, arr: np.array) -> np.array:
+        """ReLU function
+
+        Parameters
+        ----------
+        arr: np.array
+            Input array of ReLU function
+
+        Returns
+        -------
+        np.float64
+            Result of ReLU function
+        """
+        out_arr: np.array
+        out_arr = np.zeros_like(arr)
+        for batch in range(arr.shape[0]):
+            for channel in range(arr.shape[-1]):
+                for i in range(arr.shape[1]):
+                    for j in range(arr.shape[2]):
+                        if arr[batch][i][j][channel] > 0:
+                            out_arr[batch][i][j][channel] = arr[batch][i][j][channel]
+                        else:
+                            out_arr[batch][i][j][channel] = 0
         return out_arr
-    def back(self, prop: np.array):
-        pass
+    def relu_derivative(self, arr: np.array) -> np.array:
+        """ReLU derivative function
+
+        Parameters
+        ----------
+        arr: np.array
+            Input array of ReLU function
+
+        Returns
+        -------
+        np.float64
+            Result of ReLU derivative function
+        """
+        out_arr: np.array
+        out_arr = np.zeros_like(arr)
+
+        for batch in range(arr.shape[0]):
+            for channel in range(arr.shape[-1]):
+                for i in range(arr.shape[1]):
+                    for j in range(arr.shape[2]):
+                        if out_arr[batch][i][j][channel] > 0:
+                            out_arr[batch][i][j][channel] = 1
+                        else:
+                            out_arr[batch][i][j][channel] = 0
+        return out_arr
+    
+    def activation_derivative(self, weights: np.array, last_output: np.array) -> np.array:
+        """Derivative of activation funtion
+
+        Parameters
+        ----------
+        weights: np.array
+            Weights connecting previous layer with current
+        last_output: np.array
+            Output of last layer
+        Returns
+        -------
+        np.array
+            Derivative of activation function
+        """
+        out_arr: np.array
+        out_arr = np.zeros((last_output.shape[0],  self.output_dims[1], self.output_dims[2], weights.shape[0], weights.shape[1], weights.shape[2], weights.shape[3]))
+
+        # Empty array containing output of derivative 
+        #print(out_arr.shape)
+        for batch in range(out_arr.shape[0]):
+            # Iterate through all batches
+            for i in range(out_arr.shape[1]):
+                # Iterate through first dim of output
+                for j in range(out_arr.shape[2]):
+                    # Second dim of output
+                    for k in range(out_arr.shape[3]):
+                        # First dim of filter
+                        for l in range(out_arr.shape[4]):
+                            # Second dim of filter
+                                for m in range(out_arr.shape[5]):
+                                # Third dim of filter
+                                    for channel in range(out_arr.shape[6]):
+                                        # Channels
+                                        out_arr[batch][i][j][k][l][m][channel] = last_output[batch][k][l][m]
+                                        #print(last_output.shape)
+                                
+        return out_arr
+        # Return average of derivatives for every data in batch

zadanie3/conv_net/my_conv/layers/dense.py
@@ -33,8 +33,6 @@ class Dense(Layer):
         out_arr = np.zeros(out_dims)
         
         for i, data in enumerate(input):
-            print(weights.shape)
-            print(data.shape)
             self.activation_val[i] = np.matmul(weights, data) + bias
         out_arr = self.activation_fcn(self.activation_val)
         self.output = out_arr
@@ -105,17 +103,17 @@ class Dense(Layer):
         gradients = np.zeros_like(net_activation_derivative)
         prop: np.array
         prop = np.multiply(loss_derivative, net_out_derivative)
+        
         # It will propagate further. Later we return this
         for k in range(gradients.shape[0]):
             for i in range(gradients.shape[1]):
                 for j in range(gradients.shape[2]):
                     gradients[k][i][j] = net_activation_derivative[k][i][j] * prop[k][i] 
-        weights = weights - lr*gradients
         # Update weights
         
         #bias TBD
-
-        return prop
+        # print(gradients)
+        return gradients, prop
         
     def back(self, prop: np.array, prev_output: np.array, weights: np.array, bias: np.array, next_weights: np.array, lr: np.float64):
         loss_derivative: np.array
@@ -137,17 +135,13 @@ class Dense(Layer):
         # c) Calculate delta(activation)/delta(weights)
 
         prop = np.multiply(loss_derivative, out_derivative)
-
+        
         gradients = np.zeros_like(activation_derivative)
         for k in range(gradients.shape[0]):
             for i in range(gradients.shape[1]):
                 for j in range(gradients.shape[2]):
                     gradients[k][i][j] = activation_derivative[k][i][j] * prop[k][i] 
-        weights = weights - lr*gradients
-        #print(weights[0][0])
-        weights = weights - lr*gradients
-        #print(weights[0][0])
-        return prop
+        return gradients, prop
 
     def relu(self, arr: np.array) -> np.array:
         """ReLU function
@@ -279,7 +273,7 @@ class Dense(Layer):
         out_arr: np.array
         out_arr = np.zeros((net_out.shape[0], net_out.shape[1]))
         # print(gr_tru.shape)
-        print(gr_tru.shape)
+        
 
         for i, data in enumerate(net_out):
             y = gr_tru[i]

zadanie3/conv_net/my_conv/layers/flatten.py
@@ -25,16 +25,17 @@ class Flatten(Layer):
         return out_arr
     def back(self, prop: np.array, prev_output: np.array, next_weights: np.array, lr: np.float64):
         loss_derivative: np.array
-        loss_derivative = np.zeros(self.output_dims[1])
-        out_derivative: np.array
-        activation_derivative: np.array
-
-        for i in range(len(loss_derivative)):
-            # For all neurons of current layer
-            for j in range(len(prop)):
-                # Loss propagating from next layer
-                loss_derivative[i] += next_weights[j][i] * prop[j]
-                # Temp_der times weight connecting it to our neuron 
+        loss_derivative = np.zeros(self.output_dims)
+        # out_derivative: np.array
+        # activation_derivative: np.array
+        # print(next_weights.shape)
+        for data in range(prop.shape[0]):
+            for i in range(len(loss_derivative)):
+                # For all neurons of current layer
+                for j in range(len(prop)):
+                    # Loss propagating from next layer
+                    loss_derivative[data][i] += next_weights[j][i] * prop[data][j]
+                    # Temp_der times weight connecting it to our neuron 
 
         # out_derivative = self.activation_val
         # b) Calculate delta(lay_out)/delta(activation)
@@ -42,6 +43,6 @@ class Flatten(Layer):
         # activation_derivative = self.activation_derivative(weights, prev_output)
         # c) Calculate delta(activation)/delta(weights)
         #print(prev_output.shape)
-        prop = np.reshape(loss_derivative, (prev_output.shape[1], prev_output.shape[2], -1))
-        print(prop.shape)
+        prop = np.reshape(loss_derivative, (prev_output.shape[0], prev_output.shape[1], prev_output.shape[2], -1))
+        #print(prop.shape)
         return prop

zadanie3/conv_net/my_conv/layers/max_pooling.py
@@ -46,13 +46,23 @@ class MaxPooling(Layer):
         self.activation_val = out_arr
         return out_arr
     def back(self, prop: np.array, prev_output: np.array, next_weights: np.array, lr: np.float64):
+        print("Start max pooling")
         inverse_max_pooling:np.array
-        inverse_max_pooling = np.zeros(prev_output.shape[1:])
-        print(next_weights.shape)
-        for channel in range(inverse_max_pooling.shape[-1]):
-            for i in range(inverse_max_pooling.shape[0]):
-                for j in range(inverse_max_pooling.shape[1]):
-                    for k in range(self.n):
-                        for l in range(self.n):
-                            if self.output[batch][i][j][ch]:
-                                pass
+        #print(self.input_dims)
+        inverse_max_pooling = np.zeros(self.input_dims)
+        #print(prop.shape)
+        #print(next_weights.shape)
+        for batch in range(inverse_max_pooling.shape[0]): 
+            for channel in range(inverse_max_pooling.shape[3]):
+                for i in range(self.output_dims[1]): #neuron num
+                    for j in range(self.output_dims[2]): #neuron num
+                        for k in range(self.n): #slide x pooling
+                            for l in range(self.n): #slide y pooling
+                                for x in range(next_weights.shape[0]): #slide x conv
+                                    for y in range(next_weights.shape[1]): #slide y conv
+                                        if self.output[batch][i][j][channel] == self.input[batch][k+i*2][l+j*2][channel]:
+                                            # print(inverse_max_pooling.shape)
+                                            # print(prop.shape)
+                                            # print(self.output_dims)
+                                            inverse_max_pooling[batch][k+i*2][l+j*2][channel] += prop[batch][i][j][channel]
+        return inverse_max_pooling

zadanie3/conv_net/my_conv/my_conv_network.py
@@ -84,15 +84,15 @@ class MyConvNetwork:
         self.layers = []
         self.batch_size: int
         self.batch_size = batch_size
-        self.layers.append(Conv((batch_size, 28, 28, 1), (batch_size, 26, 26, 32)))
+        self.layers.append(Conv("relu", (batch_size, 28, 28, 1), (batch_size, 26, 26, 32)))
         # First conv layer
         self.layers.append(MaxPooling((batch_size, 26, 26, 32), (batch_size, 13, 13, 32)))
         # Max pooling layer to reduce size
-        self.layers.append(Conv((batch_size, 13, 13, 32), (batch_size, 11, 11, 64)))
+        self.layers.append(Conv("relu", (batch_size, 13, 13, 32), (batch_size, 11, 11, 64)))
         # Second conv layer
         self.layers.append(MaxPooling((batch_size, 11, 11, 64), (batch_size, 5, 5, 64)))
         # Max pooling layer to reduce size
-        self.layers.append(Conv((batch_size, 5, 5, 64), (batch_size, 3, 3, 64)))
+        self.layers.append(Conv("relu", (batch_size, 5, 5, 64), (batch_size, 3, 3, 64)))
         # Third conv layer
         self.layers.append(Flatten((batch_size, 3, 3, 64), (batch_size, 576)))
         # Flatten layer to prepare data for Dense layers
@@ -161,22 +161,33 @@ class MyConvNetwork:
         c = 0
         for x, layer in enumerate(self.layers[::-1]):
             if x is 0:
-                prop = layer.back_first(self.layers[-1*(x+2)].output, self.weights[-1*(c+1)], self.bias[-1*(c+1)], train_labels, self.lr)
+                gradients, prop = layer.back_first(self.layers[-1*(x+2)].output, self.weights[-1*(c+1)], self.bias[-1*(c+1)], train_labels, self.lr)
+                self.weights[-1*(c+1)] = self.weights[-1*(c+1)] - self.lr * np.mean(gradients, axis=0, dtype=np.float64)
                 c += 1
+            elif x is 7:
+                gradients, prop = layer.back(prop,  self.layers[-1*(x+1)].input, self.weights[-1*(c+1)], self.bias[-1*(c+1)], train_labels, self.lr)
+                self.weights[-1*(c+1)] = self.weights[-1*(c+1)] - self.lr * np.mean(gradients, axis=0, dtype=np.float64)
+                
+                c += 1    
             else:
                 if layer.name is "conv":
-                    #prop = layer.back(prop)
+                    print("fasfs")
+                    print(prop)
+                    gradients, prop = layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*(c+1)], self.bias[-1*(c+1)], self.weights[-1*c], self.lr)
+                    self.weights[-1*(c+1)] = self.weights[-1*(c+1)] - self.lr * np.mean(gradients, axis=0, dtype=np.float64)
                     c += 1
-                    pass 
                 elif layer.name is "dense":
-                    prop = layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*(c+1)], self.bias[-1*(c+1)], self.weights[-1*c], self.lr)
+                    gradients, prop = layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*(c+1)], self.bias[-1*(c+1)], self.weights[-1*c], self.lr)
+                    self.weights[-1*(c+1)] = self.weights[-1*(c+1)] - self.lr * np.mean(gradients, axis=0, dtype=np.float64)
                     c += 1
-                    pass
                 elif layer.name is "flatten":
-                    # prop = layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*(x)], self.lr)
+                    #print(prop.max())
+                    prop = layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*(x)], self.lr)
+                    print("flat")
+                    print(prop)
                     pass
                 elif layer.name is "max_pooling":
-                    # layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*c], self.lr)
+                    prop = layer.back(prop, self.layers[-1*(x+2)].output, self.weights[-1*c], self.lr)
                     pass
                 else:
                     print("Layer not found!")